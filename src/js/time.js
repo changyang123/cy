201df1d4c2d610ba083ce14dfa2ef9e6b416edd0
91364 <913646890@qq.com>
2018-06-11 15:49:02 +0800

    修改部分样式问题

--- src/js/time.js
+++ src/js/time.js
@@ -16,8 +16,8 @@ $.fn.datetimepicker.dates['zh'] = {
  * */
 function timesetting(element) {
     $(element).datetimepicker({
-        minView: "day", //选择日期后，不会再跳转去选择时分秒
-        format: "yyyy-mm-dd hh：ii", //选择日期后，文本框显示的日期格式
+        minView: "month", //选择日期后，不会再跳转去选择时分秒
+        format: "yyyy-mm-dd", //选择日期后，文本框显示的日期格式
         language: 'zh', //汉化
         autoclose: true, //选择日期后自动关闭
         todayBtn: false,
